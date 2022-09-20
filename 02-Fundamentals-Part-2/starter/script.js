14b179875d0a8c8ed6fffc9a09e7b08d53db4925
xiaomai <278317764@qq.com>
2022-09-20 17:26:55 +0800

    use strict

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let hasDriverLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriverLicense) console.log(`I can drive`);
+
+// const interface = "Audio";
+// const private = 534;
+// const if = 23;
