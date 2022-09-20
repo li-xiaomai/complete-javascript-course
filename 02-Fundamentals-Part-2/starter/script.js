a7326f2b9027e9c6d320ab3bd0da3b95f8c16767
xiaomai <278317764@qq.com>
2022-09-20 17:38:48 +0800

    function statement

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+/*
 let hasDriverLicense = false;
 const passTest = true;
 
@@ -9,3 +10,24 @@ if (hasDriverLicense) console.log(`I can drive`);
 // const interface = "Audio";
 // const private = 534;
 // const if = 23;
+*/
+
+function logger() {
+  console.log(`My name is Jonas`);
+}
+// calling
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+// console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
