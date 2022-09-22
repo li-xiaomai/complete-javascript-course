0109a116ba019ac6f00f187963b47bbe09e10e67
xiaomai <278317764@qq.com>
2022-09-22 13:07:15 +0800

    challenge

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -111,3 +111,14 @@ const caclaTemperaturesBug = function (temp1, temp2) {
 caclaTemperaturesBug([1, 2, 3], [-1, 3, 4, 6]);
 
 */
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+const printForecast = function (arr) {
+  let str = '...';
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]} in ${i + 1} days ... `;
+  }
+  return str;
+};
+console.log(printForecast(data2));
