f043bea2ef3ace1f62b4e06a690ebe07169fbe2c
xiaomai <278317764@qq.com>
2022-09-24 13:13:01 +0800

    challenge ... rest && || ?? ||= ??= &&=

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -309,6 +309,7 @@ const guests = restaurant.numGuests || 10;
 console.log(guests); //23
 */
 
+/*
 const rest1 = {
   name: 'xiaomai',
   numGuests: 0,
@@ -333,3 +334,81 @@ rest2.numGuests ??= '<ANONYMOUS>';
 rest1.owner &&= '<ANONYMOUS>'; //{name: 'xiaomai', numGuests: 0}
 rest2.owner &&= '<ANONYMOUS>'; //{name: 'xiaozhong', owner: '<ANONYMOUS>', numGuests: '<ANONYMOUS>'}
 console.log(rest1, rest2);
+*/
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+//1
+const {
+  players: [players1, players2],
+} = game;
+console.log(players1, players2);
+//2
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+//3
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+//4
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+//5
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+//6
+const printGoals = function (...players) {
+  console.log(players);
+};
+printGoals('xiaomai', 'xiaoli', 'xiaozhong', 'xiaoqian');
+printGoals('xiaomai', 'xiaoli');
+printGoals('xiaomai', 'xiaoli', 'xiaozhong');
+printGoals(...game.scored);
+//7 找出三个数据里面的最小值
+const win =
+  (draw < team1 && draw < team2 && 'draw') ||
+  (team2 < draw && team2 < team1 && 'team2') ||
+  (team1 < draw && team1 < team2 && 'team1');
+console.log(win);
+
+const [a, b, c] = [20, 12, 45];
